ec175c984042cb2183f5c3eaef3b6a4086ee914c
kelompok4 <>
2023-12-13 11:56:28 +0700

    controllers artikel

---
+++ application/controllers/Artikel.php
@@ -0,0 +1,4 @@
+<?php
+class Artikel extends CI_Controller {
+    // Fungsi-fungsi untuk menampilkan dan mengelola artikel
+}
---
+++ application/models/artikel_model.php
@@ -0,0 +1,4 @@
+<?php
+class Artikel_model extends CI_Model {
+    // Fungsi-fungsi untuk mengakses dan memanipulasi data artikel
+}
